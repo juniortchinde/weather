934d038381fc2c759405b61c39a79855a09f08a3
Cyrille TCHINDE <tchindec111@gmail.com>
2026-05-27 15:54:26 +0200

    extraction du fichier json

--- json/json_source.go
+++ json/json_source.go
@@ -1,24 +1,54 @@
 package json
 
-type JsonStation struct {
-	country  string
-	altitude int
-	obs      Observation
-	location Coordinate
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Stations struct {
+	Stations []Station `json:"stations"`
+}
+type Station struct {
+	Country  string        `json:"country"`
+	Altitude int           `json:"altitude_m"`
+	Obs      []Observation `json:"observations"`
+	Device   Device        `json:"device"`
+	Location Coordinate    `json:"location"`
+	Notes    *string       `json:"notes"`
 }
 
-type Coordinate struct {
-	lat  float64
-	long float64
+type Device struct {
+	Model        string `json:"type"`
+	Manufacturer string `json:"manufacturer"`
+	InstalledOn  string `json:"installed_on"`
 }
-
 type Observation struct {
-	Temperature float64
-	Condition   string
-	Wind        Wind
+	Temperature float64 `json:"temperature_celsius"`
+	Conditions  string  `json:"conditions"`
+	Wind        Wind    `json:"wind"`
 }
 
 type Wind struct {
-	Speed     float64
-	direction float64
+	Speed     float64 `json:"speed_kmh"`
+	Direction float64 `json:"direction_deg"`
+}
+type Coordinate struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+func ExtractJson(path string) (stations Stations, err error) {
+	raw, err := os.ReadFile(path)
+
+	if err != nil {
+		return
+	}
+	var station Stations
+
+	if err = json.Unmarshal(raw, &station); err != nil {
+		fmt.Println("error:", err)
+		return station, err
+	}
+	return station, nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"weather/json"
+)
+
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
 func main() {
- 
+
+	jsonStations, _ := json.ExtractJson("./data/weather_data.json")
+
+	for _, jsonStation := range jsonStations.Stations {
+		fmt.Println(jsonStation.Country)
+	}
+	fmt.Println(len(jsonStations.Stations))
 }
