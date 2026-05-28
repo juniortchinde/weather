1f8cd053c81f9fe10bb91d2920f753b0aaa47eb9
Cyrille TCHINDE <tchindec111@gmail.com>
2026-05-28 16:24:45 +0200

    feat: PUT /stations/{id} replaces or creates

--- jour3/handlers.go
+++ jour3/handlers.go
@@ -58,3 +58,28 @@ func (a *App) createStation(w http.ResponseWriter, r *http.Request) {
 	a.store.Put(station)
 	writeJSON(w, http.StatusCreated, station)
 }
+
+func (a *App) updateStation(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	id := r.PathValue("id")
+	var st Station
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	if err := dec.Decode(&st); err != nil {
+		writeError(w, 400, "JSON invalide: "+err.Error())
+		return
+	}
+	if st.Id != "" && st.Id != id {
+		writeError(w, 400, "incohérence id body vs URL")
+		return
+	}
+	
+	st.Id = id
+	created := !a.store.Has(id)
+	a.store.Put(st)
+	if created {
+		writeJSON(w, 201, st)
+		return
+	}
+	writeJSON(w, 200, st)
+}
